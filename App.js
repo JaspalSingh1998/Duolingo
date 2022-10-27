b96ab2bb4000aa0056b97c8a1aeb08f6477cf2fe
= <=>
2022-10-27 10:09:49 -0400

    added props and random data

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {Text, View, Image} from 'react-native';
 import styles from './App.styles';
 import ImageOption from './src/components/ImageOption';
+import question from './assets/data/oneQuestionWithOption';
 
 const App = () => {
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>Which of these is the "glass"?</Text>
+      <Text style={styles.title}>{question.question}</Text>
       <View style={styles.optionsContainer}>
-        <ImageOption />
+        {question.options.map(option => (
+          <ImageOption image={option.image} text={option.text} key={option.id}/>
+        ))}
       </View>
     </View>
   )
--- src/components/ImageOption/ImageOption.js
+++ src/components/ImageOption/ImageOption.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {View, Image, Text} from 'react-native';
 import styles from './styles';
 
-const ImageOption = () => {
+const ImageOption = ({image, text}) => {
     return (
       <View style={styles.optionContainer}>
-        <Image resizeMode='contain' style={styles.optionImage} source={{uri: "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png"}}/>
-        <Text style={styles.optionText}>Glass</Text>
+        <Image resizeMode='contain' style={styles.optionImage} source={{uri: image}}/>
+        <Text style={styles.optionText}>{text.toUpperCase()}</Text>
       </View>
     )
 }
